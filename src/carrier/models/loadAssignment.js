8da6bb57ee0bf5652c8bc4a04cb829bb7b79ca6e
Paulus <pwilhem@mtc.com.na>
2026-03-17 14:27:09 +0200

    Registration fix

--- src/carrier/models/loadAssignment.js
+++ src/carrier/models/loadAssignment.js
@@ -17,7 +17,7 @@ const loadAssignmentModel = sequelize.define('LoadAssignment', {
       },
     },
     carrierId: {
-      type: DataTypes.INTEGER,
+      type: DataTypes.UUID,
       allowNull: false,
       references: {
         model: "carriers",
